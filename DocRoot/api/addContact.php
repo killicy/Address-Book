ec578db22c45896a4ef3ad2685dcae35dc440a95
NoodleAlchemist <54447003+NoodleAlchemist@users.noreply.github.com>
2020-09-09 18:58:42 -0400

    added message to confirm contact was created

--- DocRoot/api/addContact.php
+++ DocRoot/api/addContact.php
@@ -14,6 +14,13 @@
 		{
 			returnWithError( $conn->error );
 		}
+		$sql = "SELECT UserId, FirstName, LastName, Phone, Address, Email FROM Contacts where UserId = '". $inData["userId"] . "' and FirstName ='" . $inData["firstName"] . "'and LastName ='" . $inData["lastName"] . "'and Phone ='" . $inData["phone"] . "'and Address='" . $inData["address"] . "'and Email ='" . $inData["email"] . "'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0)
+		{
+			returnWithInfo($inData['firstName'], $inData['lastName'], $inData['userId'] );
+		}
+
 		$conn->close();
 	}
 
@@ -34,4 +41,9 @@
 		sendResultInfoAsJson( $retValue );
 	}
 
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","Message":"Contact created"}';
+		sendResultInfoAsJson( $retValue );
+	}
 ?>
