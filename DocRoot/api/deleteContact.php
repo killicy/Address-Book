bfc3a6e62bd927d211c729007a587eec72cf14c0
Jennifer Brown <killicy@gmail.com>
2020-09-15 01:26:53 -0400

    Update API to PDO

--- DocRoot/api/deleteContact.php
+++ DocRoot/api/deleteContact.php
@@ -1,58 +1,53 @@
 <?php
 
-	$inData = getRequestInfo();
+require_once("../config.php");
+$inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "dylan_dtilley", "lf+j3_7U%SZv", "dylan_addressbook");
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
+// $sql = "SELECT ID,firstName,lastName from Contacts where FirstName like '" . $inData["firstName"] . "' and LastName like '" . $inData["lastName"] . "' and UserID like '" . $inData["userId"] . "'";
+try {
+	$stmt = $msql_connection->prepare("SELECT ContactID FROM Contacts WHERE ContactID=:contactId AND UserID=:userId");
+	$stmt->execute([
+		'userId' => $inData["userId"],
+		'contactId' => $inData["contactId"],
+	]);
+
+	$contact = $stmt->fetch();
+	
+	if (empty($contact)) {
+		returnWithError("No Records Found");
+		exit();
 	}
-	else
-	{
-		$sql = "SELECT ID,firstName,lastName from Contacts where FirstName like '" . $inData["firstName"] . "' and LastName like '" . $inData["lastName"] . "' and UserID like '" . $inData["userId"] . "'";
-		$result = $conn->query($sql);
+	else {
+		// $sql = "DELETE from Contacts where FirstName like '" . $inData["firstName"] . "' and LastName like '" . $inData["lastName"] . "' and UserID like '" . $inData["userId"] . "'";
+		$stmt = $msql_connection->prepare("DELETE FROM Contacts WHERE ContactID=:contactId AND UserID=:userId");
+		$stmt->execute([
+			'userId' => $inData["userId"],
+			'contactId' => $inData["contactId"],
+		]);
 
-		if ($result->num_rows > 0)
-		{
-			$sql = "DELETE from Contacts where FirstName like '" . $inData["firstName"] . "' and LastName like '" . $inData["lastName"] . "' and UserID like '" . $inData["userId"] . "'";
-			$conn->query($sql);
-			$sql = "SELECT from Contacts where FirstName like '" . $inData["firstName"] . "' and LastName like '" . $inData["lastName"] . "' and UserID like '" . $inData["userId"] . "'";
-			$result = $conn->query($sql);
-			if ($result->num_rows == 0){
-							returnWithInfo("Deleted contact");
-						}
-
-		}
-		else
-		{
-			returnWithError( "No Records Found" );
-		}
-		$conn->close();
+		returnWithInfo("Contact deleted");
 	}
+}
+catch (Exeception $e) {
+	returnWithError("Failed while executing query:" . $e->getMessage());
+}
 
+function getRequestInfo() {
+	return json_decode(file_get_contents('php://input'), true);
+}
 
+function sendResultInfoAsJson( $obj ) {
+	header('Content-type: application/json');
+	echo $obj;
+}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function returnWithError( $err ) {
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson( $retValue );
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+function returnWithInfo( $msg) {
+	$retValue = '{"Message":"' . $msg . '"}';
+	sendResultInfoAsJson( $retValue );
+}
 
-	function returnWithError( $err )
-		{
-			$retValue = '{"error":"' . $err . '"}';
-			sendResultInfoAsJson( $retValue );
-		}
-
-	function returnWithInfo( $msg)
-		{
-			$retValue = '{"Message":"' . $msg . '"}';
-			sendResultInfoAsJson( $retValue );
-		}
-
-?>
