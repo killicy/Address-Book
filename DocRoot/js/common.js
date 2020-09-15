8d7db18d3225b855c9a6afad829b5bbab1cd3372
Fauxfox01 <tumblingpebble@gmail.com>
2020-09-15 08:12:29 -0400

    Front end work for mainpage

--- DocRoot/js/common.js
+++ DocRoot/js/common.js
@@ -1,4 +1,6 @@
-var sideMenu = false;
+$(document).ready( function() {
+		$("#table").load("table.html");
+});
 
 function doLogin() {
 
@@ -17,12 +19,12 @@ function doLogin() {
 	}
 
 	$.post(url, JSON.stringify(jsonPayload), function(data) {
-		console.log(data.error.length);
 		if (data.error) {
 			$("#SignUpResult").html("Error: " + data.error);
 		}
 		else {
 			$("#SignUpResult").html("Success");
+			window.location.href = ("mainpage.html");
 		}
 	});
 }
@@ -36,23 +38,66 @@ function doCreate() {
 	userId = 0;
 	firstName = "";
 	lastName = "";
+
 	var firstname = $("#fname").val();
 	var lastname = $("#lname").val();
-	var login = $("#uname").val();
-	var password = $("#pword").val();
-	var jsonPayload = {login: login, password: password, firstname: firstname, lastname: lastname};
+	var login = $("#loginName").val();
+	var password = $("#loginPassword").val();
+
+	var jsonPayload = {login: login, password: password, fname: firstname, lname: lastname};
 	if((login.length == 0 || password.length == 0) || firstname.length == 0 || lastname.length == 0){
 		exit();
 	}
 
 	$.post(url, JSON.stringify(jsonPayload), function(data) {
-		if (data) {
+		if (data.error) {
 			$("#SignUpResult").html("Error: " + data.error);
 		}
 		else {
 			$("#SignUpResult").html("Success");
 			window.location.href = ("index.html");
-
 		}
 	});
+}
+
+function doCreate() {
+	var url = '/api/create.php';
+
+	var firstname = $("#fname").val();
+	var lastname = $("#lname").val();
+	var login = $("#loginName").val();
+	var password = $("#loginPassword").val();
+
+	var jsonPayload = {login: login, password: password, fname: firstname, lname: lastname};
+	if((login.length == 0 || password.length == 0) || firstname.length == 0 || lastname.length == 0){
+		exit();
+	}
+
+	$.post(url, JSON.stringify(jsonPayload), function(data) {
+		if (data.error) {
+			$("#SignUpResult").html("Error: " + data.error);
+		}
+		else {
+			$("#SignUpResult").html("Success");
+			window.location.href = ("index.html");
+		}
+	});
+}
+
+function doAdd() {
+	var url = '/api/addContact.php';
+
+	response = $.parseJSON(response);
+
+$(function() {
+    $.each(response, function(i, item) {
+        var $tr = $('<tr>').append(
+            $('<td>').text(item.rank),
+            $('<td>').text(item.content),
+            $('<td>').text(item.UID)
+        ); //.appendTo('#records_table');
+        console.log($tr.wrap('<p>').html());
+    });
+});
+
 }
